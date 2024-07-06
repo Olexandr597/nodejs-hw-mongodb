aca2dc1f9d49284fba82658f81d44d5562df7ea8
Olexandr597 <Olexandr597@gmail.com>
2024-07-06 16:43:04 +0300

    up

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,26 +7,34 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validationBody } from '../middlewares/validateBody.js';
+import {
+  contactCreateValidationSchema,
+  contactUpdateValidationSchema,
+} from '../validation/contact.js';
+import { validateMongoId } from '../middlewares/validateMongoId.js';
 
 export const contactsRouter = Router();
+contactsRouter.use('/contacts/:contactsId', validateMongoId('contactsId'));
+
 contactsRouter.get('/contacts', ctrlWrapper(getAllContactsController));
+
 contactsRouter.get(
   '/contacts/:contactsId',
   ctrlWrapper(getContactByIdController),
 );
 
-// post
-
-contactsRouter.post('/contacts', ctrlWrapper(createContactController));
-
-// patch
+contactsRouter.post(
+  '/contacts',
+  validationBody(contactCreateValidationSchema),
+  ctrlWrapper(createContactController),
+);
 
 contactsRouter.patch(
   '/contacts/:contactsId',
+  validationBody(contactUpdateValidationSchema),
   ctrlWrapper(patchContactController),
 );
-
-// delete
 
 contactsRouter.delete(
   '/contacts/:contactsId',
--- src/server.js
+++ src/server.js
@@ -1,75 +1,45 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { env } from './utils/env.js';
+import { contactsRouter } from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
-import { env } from "./utils/env.js";
-
-import { getAllContacts, getContactById } from "./services/contacts.js";
-
-const PORT = Number(env("PORT", "3000"));
-
+const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
-  app.use(express.json());
-
-  //  pino and cors
+  // add cors
   app.use(cors());
+  // add pino
   app.use(
     pino({
       transport: {
-        target: "pino-pretty",
+        target: 'pino-pretty',
       },
-    })
+    }),
   );
-  // contacts
+  // parser json
+  app.use(
+    express.json({
+      limit: '1mb',
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
 
-  app.get("/contacts", async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
+  //  get contacts & get contacts by id
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
+  // 404 middleware
+  app.use(notFoundHandler);
 
-    try {
-      const contact = await getContactById(contactId);
+  // 500 middleware
+  app.use(errorHandler);
 
-      if (!contact) {
-        return res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-      }
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (e) {
-      req.log.error(e);
-      res.status(500).json({
-        status: 500,
-        message: 'Internal server error',
-      });
-    }
-  });
-
-  //   error404
-  app.use("*", (req, res) => {
-    res.status(404).json({
-      message: "Not found",
-    });
-  });
-
-  // server
+  //   start server
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
   });
-};
 
-export default setupServer;
+  return app;
+};
